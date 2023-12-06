95d06f41cb817f342feb23af9b6d777615e8c1b1
Austin Coakley <Austin.Coakley@target.com>
2023-12-05 20:42:26 -0700

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,42 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { s -> "${s.first { it.isDigit() }}${s.last { it.isDigit() }}" }.sumOf { it.toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val numberMap = mapOf(
+            "one" to '1',
+            "two" to '2',
+            "three" to '3',
+            "four" to '4',
+            "five" to '5',
+            "six" to '6',
+            "seven" to '7',
+            "eight" to '8',
+            "nine" to '9'
+        )
+        val nums = numberMap.keys
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        fun StringBuilder.notMatching(c: Char, nums: Set<String>, add: StringBuilder.() -> StringBuilder): Boolean {
+            if (c.isDigit()) clear()
+            add()
+            return firstOrNull()?.isDigit() == false && !nums.any { number ->
+                this.toString().contains(number).also {
+                    if (it) {
+                        clear()
+                        append(numberMap[number])
+                    }
+                }
+            }
+        }
+
+        return input.sumOf { s ->
+            val first = buildString { s.takeWhile { notMatching(it, nums) { append(it) } } }
+            val second = buildString { s.takeLastWhile { notMatching(it, nums) { insert(0, it) } } }
+
+            "$first$second".toInt()
+        }
+    }
 
     val input = readInput("Day01")
     part1(input).println()
